816e47b038df1deadca9f687b7cb29e2763219c0
vDekuu <84327635+vDekuu@users.noreply.github.com>
2025-04-13 01:06:43 +0200

    removed unused usings

--- NetWatchCS/Commands/ToSCommand.cs
+++ NetWatchCS/Commands/ToSCommand.cs
@@ -1,11 +1,6 @@
-﻿using DSharpPlus.Entities;
-using DSharpPlus;
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetWatchCS.Commands
 {
